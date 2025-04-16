81d0170227f29bedec6c9b87f3410fd521862a03
Diyorbek Abdurashidov <abdurashidovdiyor4@gamil.com>
2025-04-16 22:18:50 +0500

    lesson 2

---
+++ project-folder/src/app/about/page.jsx
@@ -0,0 +1,16 @@
+export const metadata = {
+    title: "About page",
+}
+import styles from "../about/style.scss";
+
+
+function About() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium quo soluta reiciendis, porro explicabo vel sequi eligendi magnam asperiores officia, quisquam odit. Nostrum repellat, provident minus voluptates quisquam ipsum non!А</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ project-folder/src/app/page.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>This is Home page</p>
+    </div>
+  );
+}
+
+export default Home;
